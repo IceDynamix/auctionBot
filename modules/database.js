490654d0b109c6054d439b217aaca625358df9ec
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-07-06 22:05:32 +0200

    Move logic to module

---
+++ modules/database.js
@@ -0,0 +1,57 @@
+const sqlite = require("sqlite3");
+const parse = require("csv-parse");
+const fs = require("fs");
+
+function initPlayersTable(db) {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS players
+        (
+            user_id            INTEGER PRIMARY KEY,
+            username           TEXT,
+            country            TEXT,
+            rank               INTEGER,
+            badges             TEXT,
+            badge_ranks        TEXT,
+            bws                NUMERIC,
+            tier               INTEGER,
+            flag               TEXT,
+            url                TEXT,
+            image              TEXT,
+            qualifier_seed     INTEGER,
+            is_captain         INTEGER,
+            has_been_auctioned INTEGER
+        ) WITHOUT ROWID
+    `);
+
+    console.log("Created fresh table");
+
+    console.log("Importing players from players.tsv");
+    fs.createReadStream("players.tsv")
+        .pipe(parse({ delimiter: "\t" }))
+        .on("data", data => {
+            db.run(
+                `
+                    INSERT INTO players (user_id, username, country, rank, badges, badge_ranks, bws, tier, flag,
+                                         url,
+                                         image)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                `, data);
+        });
+    console.log("Finished importing players from players.tsv")
+}
+
+async function connect() {
+    let db = new sqlite.Database("./database.db");
+    console.log("Connected to database");
+
+    let playerTableExists = await db.get(`
+        SELECT name
+        FROM sqlite_master
+        WHERE type = 'table'
+          AND name = 'players';`) !== undefined;
+
+    if (!playerTableExists) initPlayersTable(db);
+    return db;
+}
+
+module.exports = { connect };
