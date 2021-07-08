f366906bcf7042c3972e267bd7c84806b996a037
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-07-08 22:08:14 +0200

    Set up basic ping and echo commands

--- modules/discord.js
+++ modules/discord.js
@@ -1,13 +1,49 @@
 const Discord = require("discord.js");
 
 async function run() {
-    const client = new Discord.Client();
+    const intents = new Discord.Intents();
+    intents.add('GUILDS', 'GUILD_MESSAGES');
+    const client = new Discord.Client({ intents });
 
-    client.on("ready", () => {
-        console.log(`Logged in as ${client.user.tag}!`);
+    client.on("ready", async () => {
+        console.log(`Logged in as ${ client.user.tag }!`);
+        const data = [
+            {
+                name: "ping",
+                description: "Replies with Pong!",
+            },
+            {
+                name: "echo",
+                description: "Replies with your input!",
+                options: [{
+                    name: "input",
+                    type: "STRING",
+                    description: "The input to echo back",
+                    required: true,
+                }],
+            },
+        ];
+
+        const guild = await client.guilds.fetch("228404074721181696");
+        await guild.commands.set(data);
+    });
+
+    client.on("interactionCreate", async interaction => {
+        if (!interaction.isCommand()) return;
+
+        switch (interaction.commandName.toLowerCase()) {
+            case "ping":
+                await interaction.reply("Pong!");
+                break;
+            case "echo":
+                const input = interaction.options.get("input").value;
+                await interaction.reply(input);
+                break;
+        }
     });
 
     client.login(process.env.TOKEN).then();
 }
+
 
 module.exports = { run };
