2849cd7c7584769dbcf42c07233f1f235d841f27
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-07-09 12:37:02 +0200

    Add bids table

--- modules/database.js
+++ modules/database.js
@@ -6,20 +6,18 @@ function initPlayersTable(db) {
     db.run(`
         CREATE TABLE IF NOT EXISTS players
         (
-            user_id            INTEGER PRIMARY KEY,
-            username           TEXT,
-            country            TEXT,
-            rank               INTEGER,
-            badges             TEXT,
-            badge_ranks        TEXT,
-            bws                NUMERIC,
-            tier               INTEGER,
-            flag               TEXT,
-            url                TEXT,
-            image              TEXT,
-            qualifier_seed     INTEGER,
-            is_captain         INTEGER,
-            has_been_auctioned INTEGER
+            user_id        INTEGER PRIMARY KEY,
+            username       TEXT,
+            country        TEXT,
+            rank           INTEGER,
+            badges         TEXT,
+            badge_ranks    TEXT,
+            bws            NUMERIC,
+            tier           INTEGER,
+            flag           TEXT,
+            url            TEXT,
+            image          TEXT,
+            qualifier_seed INTEGER
         ) WITHOUT ROWID
     `);
 
@@ -50,23 +48,37 @@ function initBiddersTable(db) {
     `);
 }
 
-async function init(db) {
-    let playerTableExists = await db.get(`
+function initBidsTable(db) {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS bids
+        (
+            bid_id       INTEGER PRIMARY KEY,
+            player_id    INTEGER,
+            final_bidder TEXT,
+            currency     INTEGER DEFAULT 0,
+            start_time   TEXT,
+            end_time     TEXT
+        )
+    `);
+}
+
+function init(db) {
+    db.get(`
         SELECT name
         FROM sqlite_master
         WHERE type = 'table'
-          AND name = 'players';`) !== undefined;
+          AND name = 'players';`, (err, row) => {
+        if (!row) initPlayersTable(db);
+    });
 
-    if (!playerTableExists) initPlayersTable(db);
     initBiddersTable(db);
+    initBidsTable(db);
 }
 
 async function connect() {
     let db = new sqlite.Database("./database.db");
     console.log("Connected to database");
-
-    await init(db);
-
+    init(db);
     return db;
 }
 
