b0e664b55e87d53fb2fb9a0d8b22187cea828949
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-07-09 12:02:47 +0200

    Add setcurrency interaction

---
+++ commands/setcurrency.js
@@ -0,0 +1,38 @@
+module.exports = {
+    data: {
+        name: "setcurrency",
+        description: "Sets the currency for a single user",
+        defaultPermission: false,
+        options: [{
+            name: "amount",
+            type: "INTEGER",
+            description: "The amount of currency to set to",
+            required: true,
+        }, {
+            name: "user",
+            type: "USER",
+            description: "The user to set the currency for",
+            required: true,
+        }],
+    },
+    handler: async (interaction, db) => {
+        const { member } = interaction.options.get("user");
+        if (!member.roles.cache.get(process.env.BIDDER_ROLE_ID)) {
+            await interaction.reply("Mentioned user is not a bidder!");
+            return;
+        }
+        const amount = interaction.options.get("amount").value;
+
+        db.run(`REPLACE INTO bidders (discord_id, currency)
+                VALUES (${ member.id }, ${ amount })`)
+
+        await interaction.reply(`Set currency of ${ member.displayName } to ${ amount }`);
+    },
+    permissions: [
+        {
+            id: process.env.ADMIN_ROLE_ID,
+            type: "ROLE",
+            permission: true,
+        },
+    ],
+}
